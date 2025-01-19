427f3f5b14725acc7acad5fe2695949cf00c8d1c
Fatih <fdonmez_1512@hotmail.com>
2025-01-19 18:43:21 +0300

    feat: add todo IDs and text decoration animation

--- index.js
+++ index.js
@@ -7,12 +7,23 @@ button.addEventListener("click", todoEkle);
 function todoEkle() {
   const eklenecekValue = input.value;
   const eklenecekTodo = document.createElement("div");
+  const randomSayi = Math.floor(Math.random() * 1000);
   eklenecekTodo.className = "card";
   eklenecekTodo.innerHTML = `
-              <input type="checkbox" />
+              <input type="checkbox" id=${"checkbox-" + randomSayi} />
               <div>
-                <p>${eklenecekValue}</p>
+                <p id=${"text-" + randomSayi}>${eklenecekValue}</p>
               </div>`;
 
   todoCards[0].appendChild(eklenecekTodo);
+  const checkbox = document.getElementById("checkbox-" + randomSayi);
+  console.log(checkbox);
+  const cardtext = document.getElementById("text-" + randomSayi);
+  checkbox.addEventListener("click", () => {
+    if (cardtext.classList.contains("furkan")) {
+      cardtext.classList.remove("furkan");
+    } else {
+      cardtext.classList.add("furkan");
+    }
+  });
 }
